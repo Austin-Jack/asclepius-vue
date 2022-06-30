3e3ac9871fa7d3d951c461031025803a585f3001
monky <2283967450@qq.com>
2022-06-30 18:44:38 +0800

    feat:finish the steps of adding patientCard

--- src/components/PatientCard.vue
+++ src/components/PatientCard.vue
@@ -1,22 +1,27 @@
 <template>
-  <el-form ref="form" :model="form" :rules="pcardRules" label-width="80px">
-    <el-form-item label="姓名">
-      <el-input v-model="form.name"></el-input>
+  <el-form
+    ref="addCardform"
+    :model="cardForm"
+    :rules="pcardRules"
+    label-width="80px"
+  >
+    <el-form-item label="姓名" prop="name">
+      <el-input v-model="cardForm.name"></el-input>
     </el-form-item>
-    <el-form-item label="年龄">
-      <el-input v-model.number="form.age"></el-input>
+    <el-form-item label="年龄" prop="age">
+      <el-input v-model.number="cardForm.age"></el-input>
     </el-form-item>
     <el-form-item label="性别">
-      <el-radio-group v-model="form.sex">
-        <el-radio :label="1" >男</el-radio>
+      <el-radio-group v-model="cardForm.sex">
+        <el-radio :label="1">男</el-radio>
         <el-radio :label="0">女</el-radio>
       </el-radio-group>
     </el-form-item>
-    <el-form-item label="手机号码">
-      <el-input v-model="form.tel"></el-input>
+    <el-form-item label="手机号码" prop="tel">
+      <el-input v-model="cardForm.tel"></el-input>
     </el-form-item>
-    <el-form-item label="证件号码">
-      <el-input v-model="form.identityID"></el-input>
+    <el-form-item label="证件号码" prop="identityID">
+      <el-input v-model="cardForm.identityID"></el-input>
     </el-form-item>
     <el-form-item>
       <el-button type="primary" @click="onSubmit">确认提交</el-button>
@@ -27,39 +32,65 @@
 
 <script>
 export default {
-    data(){
-        return {
-            form: {
-                name:"",
-                age:null,
-                sex:1,
-                tel:null,
-                identityID:null
-            },
-            pcardRules:{
-              name: [
-                { required: true, message: '请输入姓名', trigger: 'blur' }
-              ],
-              age: [
-                { required: true, message: '请输入年龄', trigger: 'blur' },
-              ],
-              sex: [
+  data() {
+    var checkMobile = (rule, value, cb) => {
+      var regMobile = /^1[3|4|5|7|8][0-9]{9}$/;
+      if (regMobile.test(value)) return cb();
+      // 手机号不合法
+      cb(new Error("请输入合法手机号！"));
+    };
 
-              ],
-              tel: [
-                { required: true, message: '请输入号码', trigger: 'blur' },
-              ],
-              identityID: [
-
-              ]
-            }
-        }
+     var checkCardID = (rule, value, callback) => {
+      if (!/(^\d{15}$)|(^\d{17}(\d|X|x)$)/.test(value)) {
+        callback(new Error("身份证格式错误!"));
+      }
+      callback();
+    };
+   
+    return {
+      // 就诊卡信息
+      cardForm: {
+        // name: "",
+        // age: null,
+        // sex: 1,
+        // tel: null,
+        // identityID: null,
+        name: "刘畅",
+        age: 20,
+        sex: 0,
+        tel: "15773187573",
+        identityID: "43042120010417900X",
+      },
+      // 就诊卡的验证规则
+      pcardRules: {
+        name: [{ required: true, message: "请输入姓名", trigger: "blur" }],
+        age: [{ required: true, message: "请输入年龄", trigger: "blur" }],
+        tel: [
+          { required: true, message: "请输入号码", trigger: "blur" },
+          { validator: checkMobile, trigger: "blur" },
+        ],
+        identityID: [
+          { required: true, message: "请输入证件号", trigger: "blur" },
+          { validator: checkCardID, trigger: "blur" },
+        ],
+      },
+    };
+  },
+  methods: {
+    
+    onSubmit() {
+      this.$refs.addCardform.validate((valid) => {
+        // 提交前验证 未通过则返回
+        if (!valid) {
+          return;
+        } else {
+        // 通过验证则发送请求 添加就诊人
+        // ...
+          this.$emit("getCard")
+        } 
+      });
     },
-    methods:{
-        onSubmit(){
-            
-        }
-    }
+  },
 };
 </script>
 
@@ -68,4 +99,3 @@ export default {
   width: 180px;
 }
 </style>
-
--- src/pages/AppointDetail.vue
+++ src/pages/AppointDetail.vue
@@ -3,41 +3,74 @@
     <NavHeader></NavHeader>
     <div class="appoint_box">
       <div class="appoint_form">
-        <div class="title">确认挂号信息</div>
+        <div class="title">提交挂号信息</div>
         <div class="form_main">
           <el-card class="box-card">
             <div class="item">医院：{{ appointForm.hospital }}</div>
             <div class="item">科室：{{ appointForm.department }}</div>
             <div class="item">医生：{{ appointForm.doctorName }}</div>
             <div class="item">
-              门诊类型：
+              <!-- 门诊类型：
               <el-radio-group v-model="appointForm.type">
                 <el-radio :label="0">普通门诊</el-radio>
                 <el-radio :label="1">专家门诊</el-radio>
                 <el-radio :label="2">国际门诊</el-radio>
-              </el-radio-group>
+              </el-radio-group> -->
+              医生职称：
+              {{ appointForm.position }}
             </div>
             <div class="item">
               门诊时间：
-              <el-select v-model="appointForm.time" placeholder="请选择就诊时间">
-                <el-option v-for="(item,index) in access_time" :key="index" :label="item" :value="item"></el-option>
+              <el-select
+                v-model="appointForm.time"
+                placeholder="请选择就诊时间"
+              >
+                <el-option
+                  v-for="(item, index) in access_time"
+                  :key="index"
+                  :label="item"
+                  :value="item"
+                ></el-option>
               </el-select>
             </div>
-            <div class="item">
-                费用：￥{{appointForm.price}}
-            </div>
-            <el-divider content-position="left">选择就诊人</el-divider>
+            <div class="item">费用：￥{{ appointForm.price }}</div>
+            <el-divider content-position="center"
+              ><span style="color: #6375b7">选择就诊人</span></el-divider
+            >
+            <el-select v-model="cid" placeholder="请选择就诊人">
+              <el-option
+                v-for="p in ofPatient"
+                :key="p.cid"
+                :label="p.name"
+                :value="p.cid"
+              ></el-option>
+            </el-select>
+            <el-button
+              type="primary"
+              plain
+              icon="el-icon-circle-plus"
+              style="margin-left: 10px"
+              @click="addCard"
+              >添加就诊人</el-button
+            >
           </el-card>
+          <el-button type="primary" class="subBtn">确认提交</el-button>
         </div>
       </div>
-      <!-- <PatientCard></PatientCard> -->
     </div>
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogVisible"
+      width="30%"
+    >
+      <PatientCard @getCard="getCardInfo"></PatientCard>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import NavHeader from "../components/NavHeader.vue";
-// import PatientCard from '../components/PatientCard.vue'
+import PatientCard from '../components/PatientCard.vue'
 
 export default {
   data() {
@@ -46,19 +79,53 @@ export default {
         hospital: "林大医院",
         department: "普通内科",
         doctorName: "吴晓波",
-        type: 0,
+        position: "主任医师",
         time: "",
         price: 30,
       },
-      access_time:[
-            "2022-6-29 周三 8:00-9:00",
-            "2022-6-30 周四 8:00-9:00"
-        ]
+      access_time: [
+        "2022-6-29 周三 8:00-9:00",
+        "2022-6-30 周四 8:00-9:00",
+        "2022-6-30 周四 8:00-9:00",
+        "2022-6-30 周四 8:00-9:00",
+        "2022-6-30 周四 8:00-9:00",
+        "2022-6-30 周四 8:00-9:00",
+        "2022-6-30 周四 8:00-9:00",
+        "2022-6-30 周四 8:00-9:00",
+      ],
+      ofPatient: [
+        {
+          cid: 10001,
+          identityID: "451423194408201455",
+          name: "徐凤英",
+          sex: 0,
+          age: 34,
+          tel: "15797887094",
+        },
+        {
+          cid: 10002,
+          identityID: "451423194408201456",
+          name: "徐小妹",
+          sex: 0,
+          age: 30,
+          tel: "15797888134",
+        },
+      ],
+      cid: null,
+      dialogVisible:false
     };
+  },
+  methods: {
+    addCard() {
+      this.dialogVisible = true
+    },
+    getCardInfo(){
+      this.dialogVisible= false
+    }
   },
   components: {
     NavHeader,
-    // PatientCard
+    PatientCard
   },
 };
 </script>
@@ -94,9 +161,18 @@ export default {
       .box-card {
         width: 600px;
         .item {
-            font-size: 14px;
-            margin-bottom: 16px;
+          font-size: 14px;
+          margin-bottom: 16px;
         }
+        .patientSelect {
+          display: flex;
+          justify-content: space-between;
+          border-bottom: 1px solid #ccc;
+        }
+      }
+      .subBtn {
+        width: 300px;
+        margin: 10px 160px;
       }
     }
   }
